8129b1ff664e86d9ad29aece40c74e92b7b57d67
ROCINANTE\dez <heav.met@yandex.ru>
2024-05-03 20:55:09 +0300

    восстановил проект полсность, кроме прогресс бара

---
+++ Classes/Comparator.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableComparator.Classes
+{
+public static class Comparator
+{
+    /// при сравнении массив цвета всегда привязан ко 1му столбцу и считается определяющим есть ли эквивалент во 2м столбце
+        private delegate void DMethodDeleg(Control control, object value);
+        private delegate void DCommonNotifyer(object progress);
+
+        public static ProgressBar ProgressBar;
+        private static DCommonNotifyer ProgressSender = value => HandleEvent(ProgressBar, value);
+
+        public static RichTextBox RichTextBox;
+        private static DCommonNotifyer EventSender = text => HandleEvent(RichTextBox, text);
+        private static List<string> _Errors = new List<string>();
+
+        public static Action CalcFinished;
+        private static Action _EventCalcFinished = CalcFinishHandler;
+
+        public static void CompareEachWithEachAndRecolor(DataGridView grid, string columnCompareMain,
+            string columnCompareSlave, string columnOut, CancellationToken cancellation)
+        {
+            ClearResultColumn(grid, columnOut);
+
+            int resColInd = 0;
+            //int emptyCellsTogether = 0;
+
+            for (int i = 0; i < grid.RowCount; i++)
+            {
+                if (string.IsNullOrEmpty(grid.Rows[i].Cells[columnCompareMain].Value?.ToString()))
+                {
+                    AddError($">Найдена пустая ячейка в первом столбце в строке {i}");
+                    continue;
+                }
+
+                bool isUniq = true;
+                for (int j = 0; j < grid.RowCount; j++)
+                {
+                    if (string.IsNullOrEmpty(grid.Rows[j].Cells[columnCompareSlave].Value?.ToString()))
+                    {
+                        AddError($">Найдена пустая ячейка во втором столбце в строке {j}");
+                        continue;
+                    }
+
+                    if (grid.Rows[i].Cells[columnCompareMain].Value.ToString() ==
+                                grid.Rows[j].Cells[columnCompareSlave].Value.ToString())
+                    {
+                        isUniq = false;
+                        break;
+                    }
+                }
+
+                if (isUniq)
+                {
+                    grid.Rows[resColInd].Cells[columnOut].Value = grid.Rows[i].Cells[columnCompareMain].Value;
+                    grid.Rows[i].Cells[columnCompareMain].Style.BackColor = Color.Green;
+                    resColInd++;
+                }
+                else
+                    grid.Rows[i].Cells[columnCompareMain].Style.BackColor = Color.Red;
+
+                ProgressSender?.Invoke(i * 100 / grid.RowCount);
+
+                if (cancellation.IsCancellationRequested)
+                {
+                    ResetCellColor(grid, columnCompareMain, Color.White);
+                    break;
+                }
+            }
+            ProgressSender?.Invoke(0);
+            ProcessErrors();
+            CalcFinished?.Invoke();
+        }
+
+        public static void CompareLineByLine(DataGridView grid, string columnCompareMain,
+            string columnCompareSlave, string columnOut, CancellationToken cancellation)
+        {
+            ClearResultColumn(grid, columnOut);
+
+            int resColInd = 0;
+            for (int i = 0; i < grid.RowCount; i++)
+            {
+                if (string.IsNullOrEmpty(grid.Rows[i].Cells[columnCompareMain].Value?.ToString()) ||
+                    string.IsNullOrEmpty(grid.Rows[i].Cells[columnCompareSlave].Value?.ToString()))
+                {
+                    AddError($">Найдена пустая ячейка в {i} строке");
+                    continue;
+                }
+
+                if (grid.Rows[i].Cells[columnCompareMain].Value.ToString() != grid.Rows[i].Cells[columnCompareSlave].Value.ToString())
+                {
+                    grid.Rows[resColInd].Cells[columnOut].Value = grid.Rows[i].Cells[columnCompareMain].Value;
+                    grid.Rows[i].Cells[columnCompareMain].Style.BackColor = Color.FromArgb(200, Color.Green);
+                    resColInd++;
+                }
+                else
+                    grid.Rows[i].Cells[columnCompareMain].Style.BackColor = Color.FromArgb(200, Color.Red);
+
+                ProgressSender?.Invoke(i * 100 / grid.RowCount);
+
+                if (cancellation.IsCancellationRequested)
+                {
+                    ResetCellColor(grid, columnCompareMain, Color.White);
+                    break;
+                }
+            }
+
+            ProgressSender?.Invoke(0);
+            ProcessErrors();
+            CalcFinished?.Invoke();
+        }
+
+        private static void ClearResultColumn(DataGridView grid, string resultColName)
+        {
+            for (int i = 0; i < grid.Rows.Count; i++)
+            {
+                grid.Rows[i].Cells[resultColName].Value = string.Empty;
+            }
+        }
+
+        private static void ResetCellColor(DataGridView grid, string nameColumn, Color color)
+        {
+            for (int i = 0; i < grid.Rows.Count; i++)
+            {
+                grid.Rows[i].Cells[nameColumn].Style.BackColor = Color.FromArgb(128, color);
+            }
+        }
+
+        private static void AddError(string message)
+        {
+            if (!_Errors.Contains(message))
+            { _Errors.Add(message); }
+        }
+
+        private static void ProcessErrors()
+        {
+            foreach (var error in _Errors)
+            {
+                EventSender?.Invoke(error);
+            }
+            _Errors.Clear();
+        }
+
+        private static void CalcFinishHandler()
+        {
+            //if (CalcFinished.Method.)
+            //{
+
+            //}
+        }
+
+
+        private static void HandleEvent(Control control, object value)
+        {
+            if (control == null) return;
+            try
+            {
+                if (control.InvokeRequired)
+                {
+                    DMethodDeleg d = new DMethodDeleg(HandleEvent);
+                    control.Invoke(d, new object[] { value });
+                }
+                else
+                {
+                    if (control.GetType() == typeof(ProgressBar))
+                    {
+                        ProgressBar.Value = (int)value;
+                    }
+                    if (control.GetType() == typeof(RichTextBox))
+                        if (!string.IsNullOrEmpty((string)value))
+                        {
+                            string time = DateTime.Now.ToString("dd.MM HH:mm:ss");
+                            StringBuilder sb = new StringBuilder(time + "  " + value);
+                            sb.AppendLine();
+                            sb.AppendLine(control.Text);
+                            control.Text = sb.ToString();
+                        }
+
+
+                    Console.WriteLine(value.ToString());
+                }
+            }
+            catch { }
+        }
+
+
+    }
+    
+}
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TableComparator.Classes;
 
 namespace TableComparator
 {
@@ -15,6 +17,185 @@ namespace TableComparator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public delegate void ErrorHandler(string message, Exception exception);
+        public static ErrorHandler ErrHandler;
+        private Thread CalcThread = null;
+        private CancellationTokenSource CancellationTokenSource = new CancellationTokenSource();
+
+        private static void PasteTSV(DataGridView grid)
+        {
+            char[] rowSplitter = { '\r', '\n' };
+            char[] columnSplitter = { '\t' };
+
+            // Get the text from clipboard
+            IDataObject dataInClipboard = Clipboard.GetDataObject();
+            string stringInClipboard = (string)dataInClipboard.GetData(DataFormats.Text);
+
+            // Split it into lines
+            string[] rowsInClipboard = stringInClipboard.Split(rowSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+            // Get the row and column of selected cell in grid
+            int r, c;
+            try
+            {
+                r = grid.SelectedCells[0].RowIndex;
+                c = grid.SelectedCells[0].ColumnIndex;
+            }
+            catch
+            {
+                r = 0; c = 0;
+            }
+
+            // Add rows into grid to fit clipboard lines
+            if (grid.Rows.Count < (r + rowsInClipboard.Length))
+            {
+                grid.Rows.Add(r + rowsInClipboard.Length - grid.Rows.Count);
+            }
+
+            // Loop through the lines, split them into cells and place the values in the corresponding cell.
+            for (int iRow = 0; iRow < rowsInClipboard.Length; iRow++)
+            {
+                // Split row into cell values
+                string[] valuesInRow = rowsInClipboard[iRow].Split(columnSplitter);
+
+                // Cycle through cell values
+                for (int iCol = 0; iCol < valuesInRow.Length; iCol++)
+                {
+
+                    // Assign cell value, only if it within columns of the grid
+                    if (grid.ColumnCount - 1 >= c + iCol)
+                    {
+                        DataGridViewCell cell = grid.Rows[r + iRow].Cells[c + iCol];
+
+                        if (!cell.ReadOnly)
+                        {
+                            cell.Value = valuesInRow[iCol];
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            grid.Columns[0].Width = (grid.Width / 3) - 15;
+            grid.Columns[1].Width = (grid.Width / 3) - 15;
+            grid.Columns[2].Width = (grid.Width / 3) - 15;
+        }
+
+        private void grid_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            int index = e.RowIndex;
+            string indexStr = (index + 1).ToString();
+            object header = this.grid.Rows[index].HeaderCell.Value;
+            if (header == null || !header.Equals(indexStr))
+                this.grid.Rows[index].HeaderCell.Value = indexStr;
+        }
+
+        private void grid_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.Shift && e.KeyCode == Keys.Insert) || (e.Control && e.KeyCode == Keys.V))
+            {
+                PasteTSV(grid);
+            }
+
+            if (e.KeyCode == Keys.Delete)
+            {
+                foreach (DataGridViewCell cell in grid.SelectedCells)
+                    cell.Value = "";
+            }
+            if (e.KeyCode == Keys.Back)
+            {
+                if (grid.SelectedRows.Count > 0)
+                {
+                    foreach (DataGridViewRow row in grid.SelectedRows)
+                    {
+                        grid.Rows.RemoveAt(row.Index);
+                        grid.Refresh();
+                    }
+                }
+                grid.Refresh();
+            }
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                PasteTSV(grid);
+            }
+        }
+
+        private void bCopyResult_Click(object sender, EventArgs e)
+        {
+            var newline = Environment.NewLine;
+            var tab = "\t";
+            var clipboard_string = "";
+            Clipboard.Clear();
+
+            for (int i = 0; i < grid.Rows.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(grid.Rows[i].Cells["col3"].Value?.ToString()))
+                    clipboard_string += grid.Rows[i].Cells["col3"].Value + newline;
+                else break;
+
+                //for (int i = 0; i < row.Cells.Count; i++)
+                //{
+                // for more cells, than 1
+                //if (i == (row.Cells.Count - 1))
+                //    clipboard_string += row.Cells[i].Value + newline;
+                //else
+                //    clipboard_string += row.Cells[i].Value + tab;
+                //}
+            }
+            try
+            {
+                Clipboard.SetText(clipboard_string);
+            }
+            catch
+            {
+                ErrHandler?.Invoke("Ошибка копирования", null);
+                return;
+            }
+        }
+
+        private void bCompare_Click(object sender, EventArgs e)
+        {
+            CancellationTokenSource = new CancellationTokenSource();
+            switch (cmFilterMethod.SelectedIndex)
+            {
+                case 0:
+                    CalcThread = new Thread(() =>
+                    {
+                        Comparator.CompareLineByLine(grid, "col1", "col2", "col3", CancellationTokenSource.Token);
+                    }); break;
+                case 1:
+                    CalcThread = new Thread(() =>
+                    {
+                        Comparator.CompareEachWithEachAndRecolor(grid, "col1", "col2", "col3", CancellationTokenSource.Token);
+                    }); break;
+            }
+            CalcThread.Start();
+            bCompare.Enabled = false;
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            if (CalcThread != null)
+            {
+                CancellationTokenSource.Cancel();
+
+                CancellationTokenSource.Dispose();
+                CalcThread = null;
+                bCompare.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Comparator.RichTextBox = this.richTextBox1;
+            //Comparator.ProgressBar = this.progressBar1;
+            Comparator.CalcFinished = bStop.PerformClick;
+
+            cmFilterMethod.SelectedIndex = 0;
         }
     }
 }
